900954df762e5b19af463815eeac3fb2edaeaeaf
Dhiraj Thakur <dhirajthakur92@facebook.com>
2013-10-13 17:33:35 +0530

    finally it's something

--- subdl.py
+++ subdl.py
@@ -16,6 +16,8 @@ import zipfile
 
 
 from BeautifulSoup import BeautifulSoup
+from operator import itemgetter
+from HTMLParser import HTMLParser 
 from requests.exceptions import ConnectionError
 
 
@@ -44,14 +46,17 @@ def main():
                         help='language of the subtitles')
     parser.add_argument('-n','--name', default='', type=unicode, nargs='?',
                         help='specify movie/tv-series name')
+    parser.add_argument('-c','--choose', action='store_true',
+                        help='choose subs from a list')
     args = parser.parse_args()
 	
     find_opensubtitles(name=args.name,
                        lang=args.lang,
-                       directory=args.dir)
+                       directory=args.dir,
+                       choose=args.choose)
 	
 
-def find_opensubtitles(name,lang,directory):
+def find_opensubtitles(name,lang,directory,choose):
 
     opensubs_lang = {
 		"all": "ALL",
@@ -140,11 +145,12 @@ def find_opensubtitles(name,lang,directory):
         if len(media_files) > 1:
             print 'Found multiple media files. Choose the file to search subs for:\n'
             for index,item in enumerate(media_files):
-                print '%s. %s\n' % (index+1,item)
-            choice = int(raw_input('Choice (number) : '))
+                print '%s. %s' % (index+1,item)
+            while choice not in range(1,len(media_files)+1):
+                choice = int(raw_input('Choice (number) : '))-1
 
         if media_files:
-            name = media_files[choice-1].lower()
+            name = media_files[choice].lower()
             replaced_name = name.replace('.',' ').replace('-',' ').split(' ')[:-1]
             search_key = ' '.join(replaced_name)
             to_exclude = re.findall(words_exclude,search_key)
@@ -157,11 +163,22 @@ def find_opensubtitles(name,lang,directory):
                "subdl --name='the help' \n")
         sys.exit(0)
 
+    sub_link = find_subs(name=name,lang=lang,choose=choose)
+    save_subs(sub_link,directory)
+    
+def find_subs(name,lang='eng',link=None,choose=False):
+
     base_url = 'http://www.opensubtitles.org'
     search_url = 'http://www.opensubtitles.org/en/search2'
 
+    if link:
+        link = base_url+link
     try:
-        pg_response = requests.get(search_url,params={'MovieName':name})
+        if link:
+            pg_response = requests.get(link)
+        else:
+            pg_response = requests.get(base_url,params={'MovieName':name,
+                                                        'SubLanguageID':lang})
     except ConnectionError:
         print "\nError: Can't connect to the internet"
         sys.exit(0)
@@ -176,21 +193,44 @@ def find_opensubtitles(name,lang,directory):
 
     table_rows = result_table.findAll('tr')
     result_rows = [ item for item in table_rows if 'name' in item.get('id','') ]
-    #print result_rows
+    
+    name_rows = []
+    for item in result_rows:
+        if item.findAll('td')[1].a['href'][-3:] == lang:
+            text = item.findAll('td')[0].findAll(text=True)
+            if len(text)==4:
+                name_rows.append(text[1].replace('\n','').replace('\t',''))
+            else:
+                name_rows.append(u'')
 
-    subs_links = [ base_url+item.findAll('td')[4].a['href'] for item in result_rows 
+    #print result_rows
+    subs_links = [ [ item.findAll('td')[0].a.string,
+                     base_url+item.findAll('td')[4].a['href']
+                   ] 
+                   for item in result_rows 
                    if item.findAll('td')[1].a['href'][-3:] == lang ]
-    #print subs_links
-    save_subs(subs_links[0],directory)
+    choice = 0
+    if choose:
+        for index,item in enumerate(subs_links):
+            print '%s. %10s - %s' % (index+1,
+                                item[0].replace('\n','').replace('\t',''),
+                                name_rows[index])
+        while choice not in range(1,len(subs_links)+1):
+            choice = int(raw_input('Choice (number) : '))-1
+
+    return subs_links[choice][1]
+
 
 def save_subs(link,directory):
+    if not link:
+        return False
     pg_response = requests.get(link)
     filename = pg_response.headers.get('content-disposition').split('=')[1].strip('"')  
     filename = os.path.join(directory,filename)
 
     with open(filename,'w') as zipf:
         zipf.write(pg_response.content)
-    print filename  
+
     with zipfile.ZipFile(filename, "r") as zipf:
         zipf.extractall(path=directory)
 
